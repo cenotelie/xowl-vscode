7b49dfb11ed86d9d3f59d6ee41e3bbe5d0077aa3
Laurent Wouters <lwouters@cenotelie.fr>
2017-07-29 12:26:00 +0200

    [feature] Language support

--- src/Main.ts
+++ src/Main.ts
@@ -51,7 +51,7 @@ export function activate(context: VSCode.ExtensionContext): void {
 function onJavaAvailable(context: VSCode.ExtensionContext, java: string, version: string): void {
     console.info("vscode-xowl-languages: Will use Java: " + java + " (version " + version + ")");
     let clientOptions: LanguageClientOptions = {
-        documentSelector: ["xowl-rdf-nt"],
+        documentSelector: ["rdf-nt"],
         synchronize: {
             fileEvents: [
                 VSCode.workspace.createFileSystemWatcher("**/*.nt")
